b2aba7d440237f1f7861b89b8abeff3599e5aa43
yar.trach <yar-trach@yar.trach>
2018-02-07 23:12:11 +0200

    [ADD] Short list JS and SCSS

--- src/javascripts/modules/list-short.js
+++ src/javascripts/modules/list-short.js
@@ -2,17 +2,26 @@ export default class ShortList {
     constructor(el) {
         this.el = el
 
-        let length = el.getElementsByTagName("li").length; 
-        console.log(length);
+        let length = el.getElementsByTagName('li').length; 
 
         if (length > 4) {
-            // var para = document.createElement("p");
-            // var node = document.createTextNode("This is new.");
-            // para.appendChild(node);
-            // el.appendChild(para);
-            // el.prependChild(para);
-
             el.classList.add('_long');
+
+            // Create wrapper
+            let wrapper = document.createElement('label');
+            wrapper.className = 'w-100';
+
+            // Create helper (checkbox input)
+            let helper = document.createElement('input');
+            helper.setAttribute('type', 'checkbox');
+            helper.className = 'list-short__helper';
+            
+            // Wrap list with label
+            el.parentNode.insertBefore(wrapper, el);
+            wrapper.appendChild(el);
+
+            // Insert helper (checkbox) before list
+            el.parentNode.insertBefore(helper, el);
         }
     }
 }
